6d0763fe1de6d98a2fc24b8ca0125968ea649ef7
RKang1 <37886862+RKang1@users.noreply.github.com>
2018-04-23 14:44:15 -0400

    Update retNFib.cs

--- retNFib.cs
+++ retNFib.cs
@@ -1,22 +1,21 @@
 public int retNFib(int input)
+{
+	int seq;
+
+	if(input < 2)
 	{
-		int seq;
-		
-		if(input < 2)
-		{
-			return input;
-		}
-		
-		int num1 = 1;
-		int num2 = 1;
-		
-		for(int i=2; i<input; i++)
-		{
-			num2 += num1;
-			num1 = num2 - num1;
-		}
-		
-		seq = num2;
-		
-		return seq;
+		return input;
+	}
+
+	int num1 = 1;
+	int num2 = 1;
+
+	for(int i=2; i<input; i++)
+	{
+		num2 += num1;
+		num1 = num2 - num1;
+	}
+
+	seq = num2;
+	return seq;
 }
